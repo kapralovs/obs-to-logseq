bbe27a273ddbb27d63fe76124349a5e0efb3699b
kasean <ser-kapralov@yandex.ru>
2024-07-13 00:33:40 +0300

    Refactoting

--- internal/note/convert.go
+++ internal/note/convert.go
@@ -2,14 +2,14 @@ package note
 
 func ConvertLinks(byteContent []byte) ([]byte, error) {
 	var (
-		content            = string(byteContent)
-		updatedContent     string
-		startLink, endLink string
-		isFullLink         bool
+		content                            = string(byteContent)
+		updatedContent, startLink, endLink string
+		isFullLink                         bool
 	)
 
 	for _, v := range content {
-		if startLink == "" && v != '[' && v != '|' && v != ']' {
+		if startLink == "" &&
+			v != '[' && v != '|' && v != ']' {
 			updatedContent += string(v)
 		}
 
@@ -19,14 +19,10 @@ func ConvertLinks(byteContent []byte) ([]byte, error) {
 
 		if startLink == "[[" {
 			if v == ']' {
-				switch endLink {
-				case "":
+				if endLink == "" || endLink == "]" {
 					endLink += string(v)
-					updatedContent += string(v)
-				case "]":
-					endLink += string(v)
-					updatedContent += string(v)
 				}
+				updatedContent += string(v)
 
 				continue
 			}
@@ -40,7 +36,7 @@ func ConvertLinks(byteContent []byte) ([]byte, error) {
 		}
 
 		if v == '[' {
-			switch endLink {
+			switch startLink {
 			case "":
 				startLink += string(v)
 				updatedContent += string(v)
